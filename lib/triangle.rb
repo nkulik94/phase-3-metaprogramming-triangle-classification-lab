6111951ae97a8c4cef83595f76bb149386dcf869
Naftali Kulik <nkulik1994@gmail.com>
2022-06-22 15:48:20 -0400

    commit tree 4f4002d336a4af9d881c0be2e065061de779e2c1

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,23 @@
 class Triangle
-  # write code here
+  attr_reader :side_1, :side_2, :side_3
+
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  def kind
+    all_sides = [@side_1, side_2, side_3].sort
+    raise TriangleError if all_sides.find { |side| side <= 0 } || all_sides[2] >= all_sides[0] + all_sides[1]
+    if all_sides.uniq.length == 1
+      :equilateral
+    elsif all_sides.uniq.length == 2
+      :isosceles
+    else :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
